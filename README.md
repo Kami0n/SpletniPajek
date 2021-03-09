b585f2a887151f6a986eccdae483024d3d301ab4
Matjaz Rupnik <mr8339@student.uni-lj.si>
2021-03-09 10:03:09 +0100

    Majhne spremebe, nic pomembnega

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # ieps-vipavska-burja
 Repozitorij za projekte IEPS.
+
+ime pajka -> fri-wier-vipavska-burja
+
+multiple workers -> več niti
+na začetku se definira število niti
+več niti ne smejo delati z isto stranjo!!
+iskanje v širino -> po celotnem nivoju naenkrat!
+
+15s na posamezen strežnik! -> na nekem IP, ne na domeni
+upoštevaj robots.txt
+sitemap in disalow
+
+pajek prenese samo HTML vsebino! -> lista tudi ostali content
+
+lahko se uporabi knjižnjice za branje robots.txt, prodobivanje spletnih strani, razčlenjevanju spletnih...
+ne sme se uporabljati knjižnjice ki ima implementirane funkcionalnosti pajka!
+
+javascript -> lahko se uporabi knjižnjica selenium
+
+
+
+začetne strani so v tabeli page, damo da so tipa frontier
+izvaja se v rangu 3-4 dni da rata 50k strani
+eni so delali tako da so naredili skupno bazo, in so vsak pri sebi pognali pajka
+ne rabimo oddat tabel image in page data
+sheme lahko razširjamo, osbtoječe sheme pa ne spreminjamo iz brišemo
+
+upošteval bo commite, ki so do roka
+če je baza prevelika lahko damo link (načeloma naj ne bi bila)
+lahko programiramo v kirem jeziku in okolju želimo, baza pa naj bo taka kot je definirana
+
+če je stran .ppt .doc .pdf lahko uznačemo kot  page data
+lahko gledamo končnice, če pa končnice ni lahko pogledamo v head kakšen je data type
+
+vaje v naprej bojo konzultacijske razen primera, če kdo na slack napiše da bi rad kakšno razlago
+
--- fri-wier-vipavska-burja.py
+++ fri-wier-vipavska-burja.py
@@ -1,23 +1,3 @@
-# ime pajka -> fri-wier-vipavska-burja
-
-# multiple workers -> več niti
-# na začetku se definira število niti
-# več niti ne smejo delati z isto stranjo!!
-# iskanje v širino -> po celotnem nivoju naenkrat!
-
-# 15s na posamezen strežnik! -> na nekem IP, ne na domeni
-# upoštevaj robots.txt
-# sitemap in disalow
-
-# pajek prenese samo HTML vsebino! -> lista tudi ostali content
-
-# lahko se uporabi knjižnjice za branje robots.txt, prodobivanje spletnih strani, razčlenjevanju spletnih...
-# ne sme se uporabljati knjižnjice ki ima implementirane funkcionalnosti pajka!
-
-# javascript -> lahko se uporabi knjižnjica selenium
-
-
-
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -25,43 +5,74 @@ import concurrent.futures
 import threading
 import psycopg2
 
-def prenasanjeVsebineStrani(webAddress):
+from reppy import Robots
+import urlcanon
+
+# inicializacija baze:
+def default_db_values():
+    conn = psycopg2.connect(host="localhost", user="user", password="friWIERvipavskaBurja")
+    conn.autocommit = True
+    
+    cur = conn.cursor()
+    cur.execute("truncate crawldb.site cascade;") # pobrisi vse vnose iz baze
+    
+    postgres_insert_query = """INSERT INTO crawldb.site (domain) VALUES (%s)"""
+    seedArray = ['gov.si','evem.gov.si','e-uprava.gov.si','e-prostor.gov.si','*.gov.si']
+    for domain in seedArray:
+        record_to_insert = (domain,)
+        cur.execute(postgres_insert_query, record_to_insert)
+    
+    cur.close()
+    conn.close()
+
+def print_db_values():
+    conn = psycopg2.connect(host="localhost", user="user", password="friWIERvipavskaBurja")
+    conn.autocommit = True
+
+    print("\nValues in the database:")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM crawldb.site")
+    for row in cur.fetchall():
+        print(f"\tCounter id: {row}")
+    cur.close()
+    conn.close()
+
+
+def prenasanjeVsebineStrani(webAddress, driver):
+    
+    # 
+    robots = Robots.fetch(webAddress+'/robots.txt')
+    print(robots.allowed(webAddress+'/pomoc', 'my-user-agent'))
+    
     print(f"Retrieving web page URL '{webAddress}'")
-    driver = webdriver.Chrome(WEB_DRIVER_LOCATION, options=chrome_options)
+    
     driver.get(webAddress)
     # Timeout needed for Web page to render (read more about it)
     time.sleep(TIMEOUT)
     html = driver.page_source
     #print(f"Retrieved Web content (truncated to first 900 chars): \n\n'\n{html[:900]}\n'\n")
     
-    # kaj moramo prepoznati?
+    
     # povezave ki so v html kodi -> href & onclick (location.href)
     # pravilno upoštevaj relativne URLje! -> načeloma piše v <head> baseurl ali og url
     # detektiranje slik <img src="">
-
-    #elements = driver.find_elements_by_tag_name("a") 
+    
     for element in driver.find_elements_by_tag_name("a"):
         href = element.get_attribute('href')
-        print(href)
-       
-    driver.close()
+        parsed_url = urlcanon.whatwg(href)
+        print(parsed_url)
+        
     
     # return URLs
     
     # vse URl se hrani v kanonični obliki -> oblika brez parametrov, napak..
-    
 
-# *.gov.si
-# gov.si,
-# evem.gov.si,
-# e-uprava.gov.si and
-# e-prostor.gov.si.
+default_db_values()
+print_db_values()
 
-# iskanje v ŠIRINO! breadth-first
-
-WEB_PAGE_ADDRESS = "http://evem.gov.si"
-WEB_DRIVER_LOCATION = "D:/Fakulteta/2 stopnja/2sem/IEPS/1seminar/chromedriver_win32/chromedriver"
+WEB_DRIVER_LOCATION = "D:/Fakulteta/2 stopnja/2sem/IEPS/1seminar/chromedriver"
 TIMEOUT = 5
+
 chrome_options = Options()
 # If you comment the following line, a browser will show ...
 chrome_options.add_argument("--headless")
@@ -70,23 +81,10 @@ chrome_options.add_argument("user-agent=fri-wier-vipavska-burja")
 
 #lock = threading.Lock()
 
-prenasanjeVsebineStrani(WEB_PAGE_ADDRESS)
+# iskanje v ŠIRINO! breadth-first -> FIFO
 
+driver = webdriver.Chrome(WEB_DRIVER_LOCATION, options=chrome_options)
 
-"""
-začetne strani so v tabeli page, damo da so tipa frontier
-izvaja se v rangu 3-4 dni da rata 50k strani
-eni so delali tako da so naredili skupno bazo, in so vsak pri sebi pognali pajka
-ne rabimo oddat tabel image in page data
-sheme lahko razširjamo, osbtoječe sheme pa ne spreminjamo iz brišemo
+prenasanjeVsebineStrani('https://gov.si', driver)
 
-upošteval bo commite, ki so do roka
-če je baza prevelika lahko damo link (načeloma naj ne bi bila)
-lahko programiramo v kirem jeziku in okolju želimo, baza pa naj bo taka kot je definirana
-
-če je stran .ppt .doc .pdf lahko uznačemo kot  page data
-lahko gledamo končnice, če pa končnice ni lahko pogledamo v head kakšen je data type
-
-vaje v naprej bojo konzultacijske razen primera, če kdo na slack napiše da bi rad kakšno razlago
-
-"""
+driver.close()
