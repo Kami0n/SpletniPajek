4bda048254ca15ced17e6a9b839a9fd64367fc70
Matjaz Rupnik <mr8339@student.uni-lj.si>
2021-03-10 13:32:13 +0100

    Timeout, robots parsing, site

--- fri-wier-vipavska-burja.py
+++ fri-wier-vipavska-burja.py
@@ -5,14 +5,18 @@ import concurrent.futures
 import threading
 import psycopg2
 
+from urllib.parse import urlparse
+
 from reppy import Robots
 import urlcanon
 
 TIMEOUT = 5
+start_time = time.time()
 
 NOTHREADS = 2
 
 WEB_DRIVER_LOCATION = "D:/Fakulteta/2 stopnja/2sem/IEPS/1seminar/chromedriver"
+
 chrome_options = Options()
 # If you comment the following line, a browser will show ...
 chrome_options.add_argument("--headless")
@@ -71,55 +75,75 @@ def getNextUrl():
     
     return url[1]
 
-def prenasanjeVsebineStrani(webAddress, driver):
+def checkRootSite(domain):
+    data = databaseGetConn("SELECT * FROM crawldb.site WHERE domain='"+domain+"'")
+    return data
+
+def fetchPageContent(webAddress, driver):
+    global start_time
+    razlikaCasa = (time.time() - start_time)
+    if razlikaCasa < TIMEOUT :
+        print("TIMEOUT")
+        time.sleep(TIMEOUT - razlikaCasa )
+    
     print(f"Retrieving content for web page URL '{webAddress}'")
     
     driver.get(webAddress)
     # Timeout needed for Web page to render (read more about it)
     time.sleep(TIMEOUT)
-    html = driver.page_source
+    content = driver.page_source
     #print(f"Retrieved Web content (truncated to first 900 chars): \n\n'\n{html[:900]}\n'\n")
     
-    # povezave ki so v html kodi -> href & onclick (location.href)
-    # pravilno upoštevaj relativne URLje! -> načeloma piše v <head> baseurl ali og url
-    # detektiranje slik <img src="">
-    
-    for element in driver.find_elements_by_tag_name("a"):
-        href = element.get_attribute('href')
-        if href:
-            parsed_url = urlcanon.whatwg(href)
-            print(parsed_url)
-        
-    
-    # return URLs
-    
-    # vse URl se hrani v kanonični obliki -> oblika brez parametrov, napak..
+    start_time = time.time()
+    return content
 
-start_time = time.time()
+
 
 seedArray = ['https://gov.si','https://evem.gov.si','https://e-uprava.gov.si','https://e-prostor.gov.si']
 initFrontier(seedArray)
 
 nextUrl = getNextUrl() #vzames prvi url iz baze
 print("Zacenjam zanko")
-while(nextUrl):
+while(nextUrl): # GLAVNA ZANKA
     
-    razlikaCasa = (time.time() - start_time)
-    if razlikaCasa < TIMEOUT :
-        print("TIMEOUT")
-        time.sleep(TIMEOUT - razlikaCasa )
-        
     
     # preveri ce stran ze imamo v site?
     
+    domain = urlparse(nextUrl).netloc
+    if not checkRootSite(domain):
+        print("NEZNAN site, Fetching Robots")
+        robotContent = fetchPageContent(nextUrl+'/robots.txt', driver)
+        robots = Robots.parse(nextUrl+'/robots.txt', robotContent)
+        print(robots)
+        
+        sitemapContent = fetchPageContent(nextUrl+'/sitemap.xml', driver)
+        
+        databasePutConn("INSERT INTO crawldb.site (domain, robots_content, sitemap_content) VALUES ('"+domain+"','"+robotContent+"','"+sitemapContent+"')")
+        
+    #else:
+        # load robots from DB
     
     
-    robots = Robots.fetch(webAddress+'/robots.txt')
-    print(robots.allowed(webAddress+'/pomoc', 'my-user-agent'))
+    #if robots.allowed(nextUrl, 'my-user-agent'):
     
     # prenesi stran
-    start_time = time.time()
-    prenasanjeVsebineStrani(nextUrl, driver)
+    #content  = fetchPageContent(nextUrl, driver)
+    
+    # povezave ki so v html kodi -> href & onclick (location.href)
+    # pravilno upoštevaj relativne URLje! -> načeloma piše v <head> baseurl ali og url
+    # detektiranje slik <img src="">
+    
+    """
+    for element in driver.find_elements_by_tag_name("a"):
+        href = element.get_attribute('href')
+        if href:
+            parsed_url = urlcanon.whatwg(href)
+            print(parsed_url)
+        
+    """
+    # return URLs
+    
+    # vse URl se hrani v kanonični obliki -> oblika brez parametrov, napak..
     
     nextUrl = getNextUrl() #vzames naslednji url iz baze
 
