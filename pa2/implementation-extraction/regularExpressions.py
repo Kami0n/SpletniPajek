034526bcf75ab647e4f0f5451026d3cd2ede099a
Matjaz Rupnik <mr8339@student.uni-lj.si>
2021-04-14 09:19:37 +0200

    Comments from instructions

--- pa2/implementation-extraction/regularExpressions.py
+++ pa2/implementation-extraction/regularExpressions.py
@@ -1,4 +1,11 @@
 
+# For each given web page type implement a separate function that will take HTML code as input.
+# The method should output extracted data in a JSON structured format to a standard output.
+# Each data item must be directly extracted using a single regular expression.
+# But you can extract multiple data items using one regular expression (this might help you when extracting optional items in list pages).
+
+
+
 def main():
     print("Hello World!")
 
--- pa2/implementation-extraction/xPath.py
+++ pa2/implementation-extraction/xPath.py
@@ -1,9 +1,13 @@
 
+# For each given web page type implement a separate function that will take HTML code as input.
+# The method should output extracted data in a JSON structured format to a standard output.
+# Each data item must be directly extracted using an XPath expression.
+# If the extracted value should be further processed, use regular expressions or other techniques to normalize them (for text only!).
+
 
 def main():
+    
     print("Hello World!")
-
-
 
 if __name__ == "__main__":
     main()
