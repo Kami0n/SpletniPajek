8033d5d0be975eba2a8a5f9bb2b7df5c69951ce4
Matjaz Rupnik <mr8339@student.uni-lj.si>
2021-04-26 19:41:25 +0200

    spremembe

--- pa2/implementation-extraction/roadRunner.py
+++ pa2/implementation-extraction/roadRunner.py
@@ -46,8 +46,7 @@ def typeOfLocation(string):
 
 def checkEndTag(start, tagsArray):
     for position in reversed(range(start)):
-        print("tag", position, tagsArray[position])
-        
+        #print("tag", position, tagsArray[position])
         if tagsArray[position].startswith("</"):
             endTag = tagsArray[position]
             return position
@@ -88,9 +87,11 @@ def roadRunner(soups, ime1, ime2):
                 endTag2 = checkEndTag(pozicija2,stran2)
                 
                 endTag = None
-                if stran1[endTag1] == stran1[endTag2]:
+                if stran1[endTag1] == stran1[endTag2]: # optional ?
                     endTag = endTag1
-                else:
+                    
+                    
+                else: # iterators (list of items)
                     print("\n\t", "ERROR: Closing tag is wrong!")
                     print("\t", "stran1:", stran1[endTag1], "stran2:", stran2[endTag2], "\n")
                 
@@ -110,8 +111,8 @@ def prepareFile(filePath, enc='utf-8'):
     html = r.read()
     
     # pobrisemo vse script, style, komentarje
-    html = re.sub(r'<script[^>]*?>([^<]*)</script>', '', html)
-    html = re.sub(r'<style[^>]*?>([^<]*)</style>', '', html)
+    html = re.sub(r'(?s)<script[^>]*?>([^<]*)</script>', '', html)
+    html = re.sub(r'(?s)<style[^>]*?>([^<]*)</style>', '', html)
     html = re.sub(r'(?s)<!--(.*)-->', '', html)
     
     html_bs = BeautifulSoup(html, 'html.parser')
@@ -126,107 +127,27 @@ def extractTest(jsonObj):
     testArray['test1'] = prepareFile(inputFolderStruct+url+'/test1.html')
     testArray['test2'] = prepareFile(inputFolderStruct+url+'/test2.html')
     roadRunner(testArray, 'test1', 'test2')
-    
 
 def extractRTV(jsonObj):
     url = 'rtvslo.si'
     rtvArray = {}
     rtvArray['audi'] = prepareFile(inputFolderStruct+url+'/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html')
     rtvArray['volvo'] = prepareFile(inputFolderStruct+url+'/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html')
-    
-    print(rtvArray)
+    roadRunner(rtvArray, 'audi', 'volvo')
 
-"""
 def extractOverstock(jsonObj):
     url = 'overstock.com'
     overstockArray = {}
     overstockArray['jewelry01'] = htmlFileRead(inputFolderStruct+url+'/jewelry01.html', 'mbcs')
     overstockArray['jewelry02'] = htmlFileRead(inputFolderStruct+url+'/jewelry02.html', 'mbcs')
-    
-    jsonObj[url] = {}
-    
-    for name,page in overstockArray.items():
-        tree = html.fromstring(page)
-        #extract title
-        titles = tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr/td/a/b')
-        # extract list price
-        listPrices = tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td[1]/table/tbody/tr[1]/td[2]/s')
-        # extract price
-        prices = tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td[1]/table/tbody/tr[1]/td[2]/s')
-        # extract saving & saving percent
-        savings = tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td[1]/table/tbody/tr[3]/td[2]/span')
-        # extract content
-        content = tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td[2]/span')
-        
-        index = 0
-        
-        tmpItems = []
-        
-        for title in titles:
-            tmpArrayItem = {}
-            
-            tmpArrayItem['title'] = title.text
-            tmpArrayItem['listPrice'] = listPrices[index].text
-            tmpArrayItem['price'] = prices[index].text
-            
-            saving = savings[index].text
-            tmpArrayItem['saving'] = saving.split(' ')[0]
-            tmpArrayItem['savingPercent'] = saving.split(' ')[1]
-            
-            tmpArrayItem['content'] = content[index].text
-            
-            tmpItems.append(tmpArrayItem)
-            
-            index += 1
-        
-        jsonObj[url][name] = tmpItems
+    roadRunner(overstockArray, 'jewelry01', 'jewelry02')
 
 def extractOwnPages(jsonObj):
     url = 'avto.net'
     avtoArray = {}
     avtoArray['Justy'] = htmlFileRead(inputFolderStruct+url+'/Justy.html', 'windows-1250')
     avtoArray['Legacy'] = htmlFileRead(inputFolderStruct+url+'/Legacy.html', 'windows-1250')
-    
-    jsonObj[url] = {}
-    
-    for name,page in avtoArray.items():
-        tree = html.fromstring(page)
-        #extract title
-        titles = tree.xpath('/html/body/strong/div[1]/div[3]/div/div[5]/form/div/div[1]/span')
-        # extract year 1 registration
-        firstRegist = tree.xpath('/html/body/strong/div[1]/div[3]/div/div[5]/form/div/div[4]/div/table/tbody/tr[1]/td[2]')
-        # extract mileage
-        kilometers = tree.xpath('/html/body/strong/div[1]/div[3]/div/div[5]/form/div/div[4]/div/table/tbody/tr[2]/td[2]')
-        # extract fuel
-        fuel = tree.xpath('/html/body/strong/div[1]/div[3]/div/div[5]/form/div/div[4]/div/table/tbody/tr[3]/td[2]')
-        # extract transmission
-        transmission = tree.xpath('/html/body/strong/div[1]/div[3]/div/div[5]/form/div/div[4]/div/table/tbody/tr[4]/td[2]')
-        # extract engine
-        engine = tree.xpath('/html/body/strong/div[1]/div[3]/div/div[5]/form/div/div[4]/div/table/tbody/tr[5]/td[2]')
-        # extract price
-        price = tree.xpath('/html/body/strong/div[1]/div[3]/div/div[5]/form/div/div[contains(@class, "GO-Results-PriceLogo")]/div[1]/div[1]/div')
-        
-        index = 0
-        
-        tmpItems = []
-        
-        for title in titles:
-            tmpArrayItem = {}
-            
-            tmpArrayItem['title'] = title.text
-            tmpArrayItem['firstRegist'] = firstRegist[index].text
-            tmpArrayItem['kilometers'] = kilometers[index].text
-            tmpArrayItem['fuel'] = fuel[index].text
-            tmpArrayItem['transmission'] = transmission[index].text
-            tmpArrayItem['engine'] = engine[index].text
-            tmpArrayItem['price'] = price[index].text
-            
-            tmpItems.append(tmpArrayItem)
-            
-            index += 1
-        
-        jsonObj[url][name] = tmpItems
-"""
+    roadRunner(avtoArray, 'Justy', 'Legacy')
 
 def exportJson(jsonText):
     f = open(outputFolderStruct+'xPathExport.json', 'wb')
