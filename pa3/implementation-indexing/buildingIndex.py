12302cfd47d9c6f6e204274251c4773b81b5e035
Matjaz Rupnik <mr8339@student.uni-lj.si>
2021-05-05 15:12:50 +0200

    reading file and tokenize words

--- pa3/implementation-indexing/buildingIndex.py
+++ pa3/implementation-indexing/buildingIndex.py
@@ -1,5 +1,52 @@
+import os
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import urllib.request
+
+from nltk.tokenize import LegalitySyllableTokenizer
+from nltk import word_tokenize
+from nltk.corpus import words
+
+from stopwords import *  
+
 import sqlite3
 conn = sqlite3.connect('inverted-index.db')
+
+def tagVisible(element):
+    if element.parent.name in ['style', 'script']: # , 'head', 'title', 'meta', '[document]'
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+def textFromHtml(body):
+    soup = BeautifulSoup(body, 'lxml')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tagVisible, texts)
+    return u" ".join(t.strip() for t in visible_texts)
+
+def prepareText(filePath, enc='utf-8'):
+    htmlFile = open(filePath, 'r', encoding=enc)
+    textStarting = textFromHtml(htmlFile.read())
+    
+    # lower case
+    textLower = textStarting.lower()
+    
+    # tokenization
+    word_tokens = word_tokenize(textLower)
+    
+    # stop word removal
+    filtered_sentence = [w for w in word_tokens if not w in stop_words_slovene] 
+    
+    return filtered_sentence
+
+
+baseDir = "../PA3-test"
+#baseDir = "../PA3-data"
+for path, subdirs, files in os.walk(baseDir):
+    for name in files:
+        htmlText = prepareText(os.path.join(path, name))
+        print(htmlText)
 
 # pridobimo besedilo iz vsake spletne strani:
 # funkcija text na vsaki spletni strani
