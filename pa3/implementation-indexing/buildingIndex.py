b817290c84d2ef215e00d8318c43f4b18f8284ce
Toncaw <licen.blaz@gmail.com>
2021-05-06 18:39:19 +0200

    Update buildingIndex.py

--- pa3/implementation-indexing/buildingIndex.py
+++ pa3/implementation-indexing/buildingIndex.py
@@ -39,36 +39,39 @@ def prepareText(filePath, enc='utf-8'):
     filtered_sentence = [w for w in word_tokens if not w in stop_words_slovene]
     return filtered_sentence
 
-def insertIndexWord(data):
-    keys = list(dict.fromkeys(htmlText))
-    c = conn.cursor()
-    for key in keys:
-        if key[0] == "'" or '"':
-            key=key[1::]
-            print(key)
-        if key[-1] == "'" or '"':
-            key=key[::-1]
-            print(key)
-        query1 = "INSERT INTO IndexWord VALUES('" + key + "');"
-
-        try:
-            c.execute(query1)
-        except sqlite3.IntegrityError:
-            pass
-
-    conn.commit()
+# def insertIndexWord(data):
+#     keys = list(dict.fromkeys(htmlText))
+#     c = conn.cursor()
+#     for key in keys:
+#         if key[0] == "'" or '"':
+#             key=key[1::]
+#             print(key)
+#         if key[-1] == "'" or '"':
+#             key=key[::-1]
+#             print(key)
+#         query1 = "INSERT INTO IndexWord VALUES('" + key + "');"
+#
+#         try:
+#             c.execute(query1)
+#         except sqlite3.IntegrityError:
+#             pass
+#
+#     conn.commit()
 
 
 #baseDir = "../PA3-test"
 baseDir = "../PA3-data"
 htmlText = []
+c = conn.cursor()
+
 for path, subdirs, files in os.walk(baseDir):
     for name in files:
         htmlText += prepareText(os.path.join(path, name))
         print(htmlText)
 
-print("### ")
-insertIndexWord(htmlText)
+for word in htmlText:
+    key = (word)
+    c.execute('INSERT INTO IndexWord VALUES (?); ', [key])
 
 
 
